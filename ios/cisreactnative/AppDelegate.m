eab81a44dfe6458b937b55bbb4112de465be5c90
dguard <av.serditov@gmail.com>
2022-01-12 12:02:35 +0500

    rename to cisreactnative

--- ios/cisreactnative/AppDelegate.m
+++ ios/cisreactnative/AppDelegate.m
@@ -33,7 +33,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"reactsnakebellmanford"
+                                                   moduleName:@"cisreactnative"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
